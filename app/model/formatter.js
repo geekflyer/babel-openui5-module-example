6891a52673605a5cd89c1d04a98b8cc514e7cd6b
Christian Theilemann <christian.theilemann@sap.com>
2015-04-06 01:56:25 +0200

    use ES6 arrow functions for formatter functions

--- app/model/formatter.js
+++ app/model/formatter.js
@@ -5,9 +5,7 @@
  * @param bIsPhone
  * @returns {sap.m.ListMode}
  */
-export var listMode = function (bIsPhone) {
-    return (bIsPhone ? "None" : "SingleSelectMaster");
-};
+export var listMode = bIsPhone => bIsPhone ? "None" : "SingleSelectMaster";
 
 /**
  * Returns Active for phone and Inactive for other devices
@@ -16,9 +14,7 @@ export var listMode = function (bIsPhone) {
  * @param bIsPhone
  * @returns {string}
  */
-export var listItemType = function (bIsPhone) {
-    return (bIsPhone ? "Active" : "Inactive");
-};
+export var listItemType = bIsPhone => bIsPhone ? "Active" : "Inactive";
 
 /**
  * Rounds the currency value to 2 digits
@@ -27,10 +23,4 @@ export var listItemType = function (bIsPhone) {
  * @param sValue
  * @returns {string}
  */
-export var currencyValue = function (sValue) {
-    if (!sValue) {
-        return "";
-    }
-
-    return parseFloat(sValue).toFixed(2);
-};
+export var currencyValue = sValue => !sValue ? '' : parseFloat(sValue).toFixed(2);
