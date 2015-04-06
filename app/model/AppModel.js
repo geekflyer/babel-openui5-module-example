bbdc63855d30cb7be5952ca0b1eab75298a54fcc
Christian Theilemann <christian.theilemann@sap.com>
2015-04-06 03:07:04 +0200

    use ES6 classes for AppModel too

--- app/model/AppModel.js
+++ app/model/AppModel.js
@@ -1,32 +1,31 @@
 import ODataModel from "sap/ui/model/odata/v2/ODataModel";
 
-export default ODataModel.extend("sap.ui.demo.mdtemplate.model.AppModel", {
-		/**
-		 * Will wait until there is data in the model for the elementbinding given.
-		 *
-		 * @param oElementBinding the element binding that should contain the data
-		 * @returns {Promise} Gets resolved if the data is already on the client or when the data has been requested from the server. Gets rejected when there was no data on the server.
-		 */
-		whenThereIsDataForTheElementBinding : function (oElementBinding) {
-			var sPath = oElementBinding.getPath(),
-				oModel = oElementBinding.getModel();
+export default class AppModel extends ODataModel {
+    /**
+     * Will wait until there is data in the model for the elementbinding given.
+     *
+     * @param oElementBinding the element binding that should contain the data
+     * @returns {Promise} Gets resolved if the data is already on the client or when the data has been requested from the server. Gets rejected when there was no data on the server.
+     */
+    whenThereIsDataForTheElementBinding(oElementBinding) {
+        var sPath = oElementBinding.getPath(),
+            oModel = oElementBinding.getModel();
 
-			return new Promise(function (fnSuccess, fnReject) {
-				//Check if the data is already on the client
-				if (!oModel.getProperty(sPath)) {
-					// Check that the object specified actually was found.
-					oElementBinding.attachEventOnce("dataReceived", function () {
-						var oData = oModel.getProperty(sPath);
-						if (!oData) {
-							fnReject();
-						} else {
-							fnSuccess(sPath);
-						}
-					}, this);
-				} else {
-					fnSuccess(sPath);
-				}
-			});
-		}
-
-	});
+        return new Promise(function (fnSuccess, fnReject) {
+            //Check if the data is already on the client
+            if (!oModel.getProperty(sPath)) {
+                // Check that the object specified actually was found.
+                oElementBinding.attachEventOnce("dataReceived", function () {
+                    var oData = oModel.getProperty(sPath);
+                    if (!oData) {
+                        fnReject();
+                    } else {
+                        fnSuccess(sPath);
+                    }
+                }, this);
+            } else {
+                fnSuccess(sPath);
+            }
+        });
+    }
+}
